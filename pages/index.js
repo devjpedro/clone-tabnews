acd10b636f8ff1c2814d74386fba9844c5e8746e
buckz <joaopedross.ti9@gmail.com>
2025-12-18 15:28:05 -0300

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Se você deseja fazer uma torta de maçã do zero, você deve, primeiro, criar o universo.</h1>;
+return <h1>Se você deseja fazer uma torta de maçã do zero, você deve, primeiro, criar o universo.</h1>;
 }
 
 export default Home;
